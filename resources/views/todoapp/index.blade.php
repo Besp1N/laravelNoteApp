d2b12d1c0b9bf607a9f8b7cb61ee8705e2e7134c
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-08-19 19:13:42 +0200

    test

---
+++ resources/views/todoapp/index.blade.php
@@ -0,0 +1,9 @@
+<h1>TO-DO LIST</h1>
+
+<form>
+    <div>
+        <input type="text" placeholder="enter a to do item" required>
+        <input type="submit" value="add">
+    </div>
+</form>
+<a href="/">HOME</a>
